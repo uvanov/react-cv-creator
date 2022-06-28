0ff7d038157c8f653d8a1ed4c1e9b3b1e333356f
uvanov <kirill.ivanov000@gmail.com>
2022-06-28 13:35:09 +0300

    feat: Install react-rewired / Configure aliases

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -5,15 +5,11 @@ import Props from './Button.typings';
 import StyledButton from './Button.styles';
 import { BUTTON_VARIANTS } from './Button.constants';
 
-const Button: React.FC<Readonly<Props>> = props => {
-  return (
-    <StyledButton
-      css={ BUTTON_VARIANTS[props.variant] }
-      { ...props }
-    >
-      { props.children }
-    </StyledButton>
-  );
-};
+const Button: React.FC<Readonly<Props>> = props => (
+  <StyledButton
+    css={ BUTTON_VARIANTS[props.variant] }
+    { ...props }
+  />
+);
 
 export default Button;
