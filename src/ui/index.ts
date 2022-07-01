0e9f7ce31bfbd54e941b7e8a9c2368c799d614ec
uvanov <kirill.ivanov000@gmail.com>
2022-07-01 16:02:06 +0300

    feat: Remove old UI-components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,13 +1,7 @@
 import Flex from './Flex/Flex';
-import Typography from './Typography/Typography';
-import Button from './Button/Button';
 import Grid from './Grid/Grid';
-import Input from './Input/Input';
 
 export {
   Flex,
-  Typography,
-  Button,
   Grid,
-  Input
 };
